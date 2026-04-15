d1b5cb6c714cb17984d26bd6d2981d899e8053ea
Aleksey Dmitriev <73853919+dmtew@users.noreply.github.com>
2026-04-15 13:14:04 +0700

    Version update

--- wp_robokassa.php
+++ wp_robokassa.php
@@ -5,7 +5,7 @@
  * Plugin URI: /wp-admin/admin.php?page=main_settings_rb.php
  * Author: Robokassa
  * Author URI: https://robokassa.com
- * Version: 1.8.5
+ * Version: 1.8.6
  */
 
 require_once('payment-widget.php');
